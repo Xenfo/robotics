40695c6d8a54be12ac08b149800b0bebe3e58043
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-11-15 16:53:44 -0500

    faster sumo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Robotics
 Python programs for robotics
+
+## Programs
+
+> Scratch programs under `spike_prime` are simply translations of Python code and likely don't work
--- spike_prime/python/sumo.py
+++ spike_prime/python/sumo.py
@@ -11,30 +11,25 @@ hub.motion_sensor.reset_yaw_angle()
 has_reached_edge = False
 
 def get_away_from_edge():
-    time_end = time.time() + 3
+    time_end = time.time() + 2
     while time.time() < time_end:
-        movement_motors.start(speed=50)
+        movement_motors.start(speed=100)
 
     yaw = hub.motion_sensor.get_yaw_angle()
     while yaw < 175 and yaw > -175:
-        movement_motors.start_tank(-20, 30)
+        movement_motors.start_tank(-100, 100)
         yaw = hub.motion_sensor.get_yaw_angle()
-
-    time_end = time.time() + 3
-    while time.time() < time_end:
-        movement_motors.start(speed=-50)
 
 while True:
     reflected_light = color_sensor.get_reflected_light()
     
     if not reflected_light <= 40 and not has_reached_edge:
-        movement_motors.start(speed=-30)
+        movement_motors.start(speed=-100)
     elif reflected_light <= 40 and not has_reached_edge:
         has_reached_edge = True
 
         get_away_from_edge()
-
-    if has_reached_edge:
+    elif has_reached_edge:
         distance = distance_sensor.get_distance_cm()
 
         if (not distance == None and distance <= 80) and not reflected_light <= 40:
@@ -42,4 +37,5 @@ while True:
         elif reflected_light <= 40:
             get_away_from_edge()
         else:
-            movement_motors.start_tank(-20, 30)
+            movement_motors.start_tank(-60, 60)
+
